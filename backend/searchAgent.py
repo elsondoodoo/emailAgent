55fca07764e288e3d792884822e3bc117b60ebcb
Elson <elsonliu_haiyot@hotmail.com>
2025-03-02 23:23:32 -0800

    Remove sensitive credentials file

--- backend/searchAgent.py
+++ backend/searchAgent.py
@@ -2,14 +2,18 @@ import os
 import re
 from dotenv import load_dotenv
 from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import HumanMessage
+from langgraph.prebuilt import create_react_agent
 
 load_dotenv('.env-local')
 # Set environmental variable for Tavily API key
 os.environ["TAVILY_API_KEY"] = os.getenv("TAVILY_API_KEY")
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
 search = TavilySearchResults(max_results=3)
 search_results = search.invoke(
-    "what are the emails of the UC Berkeley professors' emails that teach computer science? "
+    "what are the emails of the UC Berkeley professors' emails that teach cs?"
 )
 
 # Email regex pattern
@@ -26,3 +30,15 @@ for result in search_results:
 # If we want, we can create other tools.
 # Once we have all the tools we want, we can put them in a list that we will reference later.
 tools = [search]
+
+model = init_chat_model("gpt-4", model_provider="openai")
+
+model_with_tools = model.bind_tools(tools)
+response = model_with_tools.invoke([HumanMessage(content="what are UC Berkeley professors' emails that teach computer science?")])
+
+print(f"ContentString: {response.content}")
+print(f"ToolCalls: {response.tool_calls}")
+
+
+
+agent_executor = create_react_agent(model, tools)
